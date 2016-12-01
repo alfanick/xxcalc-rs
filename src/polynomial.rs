c74a6eea9d1f84fd7ff2b61ad771fe11a8277002
Amadeusz Juskowiak <amadeusz@amanointeractive.com>
2016-12-01 11:50:56 +0100

    added some doucmentation to polynomial, made basic ops non-mutable

--- src/polynomial.rs
+++ src/polynomial.rs
@@ -1,35 +1,135 @@
+/// Polynomial struct represents a mathematical concept of polynomials.
+///
+/// A polynomial is defined by real values of its coefficients corresponding
+/// to natural-numbered powers of the polynomial x. As coefficients are represented
+/// as continous vector, storing polynomials of high degree, but with most
+/// terms empty is quite expensive (`x^1024` makes vector of size 1024).
+///
+/// As one can imagine `x^3.14` is not a polynomial, while `3.14x^2+2` is a
+/// polynomial.
+///
+/// # Examples
+///
+/// ```
+/// use xxcalc::polynomial::Polynomial;
+///
+/// assert_eq!(String::from(Polynomial::new(&[1.0])), "1");
+/// assert_eq!(String::from(Polynomial::new(&[0.0, 1.0])), "x");
+/// assert_eq!(String::from(Polynomial::new(&[2.0, 0.0, 3.14])), "3.14x^2+2");
+/// ```
 #[derive(Debug, Clone)]
 pub struct Polynomial {
+  /// Vector of coefficients for appropriate powers
   coefficients: Vec<f64>
 }
 
 impl Polynomial {
+  /// Creates new polynomial from given coefficients
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// use xxcalc::polynomial::Polynomial;
+  ///
+  /// assert_eq!(Polynomial::new(&[1.0, 2.0, 3.0])[0], 1.0);
+  /// assert_eq!(Polynomial::new(&[1.0, 2.0, 3.0])[2], 3.0);
+  /// ```
   #[inline(always)]
   pub fn new(c: &[f64]) -> Polynomial {
     Polynomial { coefficients: c.to_vec() }
   }
 
+  /// Creates a linear polynomial (a polynomial of degree one).
+  /// Such polynomial represents a linear expression `ax+b`.
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// use xxcalc::polynomial::Polynomial;
+  ///
+  /// assert_eq!(Polynomial::linear(1.0, 0.0), Polynomial::new(&[1.0, 0.0]));
+  /// assert_eq!(Polynomial::linear(0.0, 1.0), Polynomial::new(&[0.0, 1.0]));
+  /// ```
   #[inline(always)]
   pub fn linear(b: f64, a: f64) -> Polynomial {
     Polynomial { coefficients: vec![b, a] }
   }
 
+  /// Creates a constant (degenerative) polynomial (a polynomial of
+  /// degree zero).
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// use xxcalc::polynomial::Polynomial;
+  ///
+  /// assert_eq!(Polynomial::constant(0.0), Polynomial::new(&[0.0]));
+  /// assert_eq!(Polynomial::constant(3.14), Polynomial::new(&[3.14]));
+  /// ```
   #[inline(always)]
   pub fn constant(b: f64) -> Polynomial {
     Polynomial { coefficients: vec![b] }
   }
 
+  /// Creates a zero polynomial (a polynomial with constant 0.0). While
+  /// the math does not define degree of such polynomial, for sake of
+  /// usability, in this library such polynomial is defined with a degree zero.
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// use xxcalc::polynomial::Polynomial;
+  ///
+  /// assert_eq!(Polynomial::zero(), Polynomial::constant(0.0));
+  /// assert_eq!(Polynomial::zero().degree(), 0);
+  /// ```
   #[inline(always)]
   pub fn zero() -> Polynomial {
     Polynomial { coefficients: vec![0.0] }
   }
 
+  /// Computes degree of the polynomial. This method has a linear
+  /// complexity as degree is not stored precomputed.
+  ///
+  /// Degree is a largest power with non-zero coefficient, however
+  /// a zero polynomial has a zero degree. It should be observed
+  /// that size of coefficients vector may not be equal to the degree,
+  /// as some of coefficients may be zero.
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// use xxcalc::polynomial::Polynomial;
+  ///
+  /// assert_eq!(Polynomial::zero().degree(), 0);
+  /// assert_eq!(Polynomial::constant(0.0).degree(), 0);
+  /// assert_eq!(Polynomial::linear(1.0, 0.0).degree(), 0);
+  /// assert_eq!(Polynomial::linear(1.0, 1.0).degree(), 1);
+  /// assert_eq!(Polynomial::new(&[1.0, 1.0, 1.0, 1.0]).degree(), 3);
+  /// assert_eq!(Polynomial::new(&[1.0, 1.0, 1.0, 1.0, 0.0, 0.0]).degree(), 3);
+  /// ```
   pub fn degree(&self) -> usize {
     self.coefficients.iter()
                      .rposition(|&a| a != 0.0)
                      .unwrap_or(0)
   }
 
+  /// Computes value of the polynomial by binding the variable with
+  /// given constant value.
+  ///
+  /// A Horner method is used for this computation to keep it linear
+  /// and efficient.
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// use xxcalc::polynomial::Polynomial;
+  ///
+  /// assert_eq!(Polynomial::constant(3.0).bind(9.0), Polynomial::constant(3.0));
+  /// assert_eq!(Polynomial::linear(0.0, 1.0).bind(2.0), Polynomial::constant(2.0));
+  /// assert_eq!(Polynomial::linear(1.0, 4.0).bind(2.0), Polynomial::constant(9.0));
+  /// assert_eq!(Polynomial::new(&[1.0, 4.0, 4.0]).bind(2.0), Polynomial::constant(25.0));
+  /// ```
   pub fn bind(&self, x: f64) -> Polynomial {
     let mut result: f64 = 0.0;
 
@@ -41,6 +141,29 @@ impl Polynomial {
     Polynomial::constant(result)
   }
 
+  /// Tries to interpret polynomial as real value.
+  ///
+  /// Polynomial can be transformed to real value (double) only
+  /// if its degree is zero (if the polynomial is constant),
+  /// otherwise value of polynomial can be binded for particular
+  /// value of variable.
+  ///
+  /// # Errors
+  ///
+  /// Will return `PolynomialError::NonConstantError` when the degree
+  /// is greater than zero.
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// use xxcalc::polynomial::Polynomial;
+  /// use xxcalc::polynomial::PolynomialError;
+  ///
+  /// assert_eq!(Polynomial::constant(3.0).as_f64(), Ok(3.0));
+  /// assert_eq!(Polynomial::zero().as_f64(), Ok(0.0));
+  /// assert_eq!(Polynomial::linear(1.0, 0.0).as_f64(), Ok(1.0));
+  /// assert_eq!(Polynomial::linear(1.0, 1.0).as_f64(), Err(PolynomialError::NonConstantError));
+  /// ```
   pub fn as_f64(&self) -> Result<f64, PolynomialError> {
     match self.degree() {
       0 => Ok(self.coefficients[0]),
@@ -48,6 +171,26 @@ impl Polynomial {
     }
   }
 
+  /// Returns human readable representation of the polynomial.
+  /// Uses provided name as name of the variable.
+  ///
+  /// The returned string is as short as possible version of
+  /// the polynomial starting with the largest exponents. Terms
+  /// with empty coefficients are skipped, so as unnecessary
+  /// symbols and exponents. In example `2x+1` is returned instead
+  /// of `0*x^2+2*x^2+2*x^0`.
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// use xxcalc::polynomial::Polynomial;
+  ///
+  /// assert_eq!(Polynomial::new(&[0.0, 2.0]).as_string("x"), "2x");
+  /// assert_eq!(Polynomial::new(&[-1.0, 2.0]).as_string("x"), "2x-1");
+  /// assert_eq!(Polynomial::new(&[-1.0, 2.0, 4.0]).as_string("x"), "4x^2+2x-1");
+  /// assert_eq!(Polynomial::new(&[-1.0, 2.0, 0.0, 4.0]).as_string("x"), "4x^3+2x-1");
+  /// assert_eq!(Polynomial::new(&[-1.0, 2.0, 0.0, 4.0]).as_string("foo"), "4foo^3+2foo-1");
+  /// ```
   pub fn as_string(&self, name: &str) -> String {
     self.coefficients.iter().enumerate().rev()
                      .filter_map(|(exponent, &coefficient)| {
@@ -79,6 +222,15 @@ impl Polynomial {
   }
 }
 
+/// A zero polynomial is returned by default
+///
+/// # Examples
+///
+/// ```
+/// use xxcalc::polynomial::Polynomial;
+///
+/// assert_eq!(Polynomial::default(), Polynomial::zero());
+/// ```
 impl Default for Polynomial {
   #[inline(always)]
   fn default() -> Polynomial {
@@ -86,8 +238,34 @@ impl Default for Polynomial {
   }
 }
 
+/// Makes string out of polynomial with `x` symbol
+/// as a variable.
+///
+/// # Examples
+///
+/// ```
+/// use xxcalc::polynomial::Polynomial;
+///
+/// assert_eq!(String::from(Polynomial::linear(1.0, 2.0)), "2x+1");
+/// ```
+impl From<Polynomial> for String {
+  fn from(p: Polynomial) -> String {
+    p.as_string("x")
+  }
+}
+
 use std::fmt;
 
+/// Implements Display trait with `x` symbol as a variable.
+///
+/// # Examples
+///
+/// ```
+/// use xxcalc::polynomial::Polynomial;
+///
+/// // prints expr: 2x+1
+/// println!("expr: {}", Polynomial::linear(1.0, 2.0));
+/// ```
 impl fmt::Display for Polynomial {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
     f.write_str(self.as_string("x").as_str())
@@ -139,8 +317,10 @@ impl AddAssign for Polynomial {
 impl Add for Polynomial {
   type Output = Polynomial;
 
-  fn add(mut self, other: Polynomial) -> Polynomial {
-    (&mut self + &other).clone()
+  fn add(self, other: Polynomial) -> Polynomial {
+    let mut c = self.clone();
+    c += other;
+    c
   }
 }
 
@@ -169,8 +349,10 @@ impl SubAssign for Polynomial {
 impl Sub for Polynomial {
   type Output = Polynomial;
 
-  fn sub(mut self, other: Polynomial) -> Polynomial {
-    (&mut self - &other).clone()
+  fn sub(self, other: Polynomial) -> Polynomial {
+    let mut c = self.clone();
+    c -= other;
+    c
   }
 }
 
@@ -215,8 +397,10 @@ impl<'a, 'b> MulAssign<&'b Polynomial> for &'a mut Polynomial {
 impl Mul for Polynomial {
   type Output = Polynomial;
 
-  fn mul(mut self, other: Polynomial) -> Polynomial {
-    (&mut self * &other).clone()
+  fn mul(self, other: Polynomial) -> Polynomial {
+    let mut c = self.clone();
+    c *= other;
+    c
   }
 }
 
@@ -285,8 +469,10 @@ impl DivAssign for Polynomial {
 impl Div for Polynomial {
   type Output = Polynomial;
 
-  fn div(mut self, other: Polynomial) -> Polynomial {
-    (&mut self / &other).clone()
+  fn div(self, other: Polynomial) -> Polynomial {
+    let mut c = self.clone();
+    c /= other;
+    c
   }
 }
 
