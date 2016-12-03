522ac4a5d5afa58b2235e4db14fd67598bb79fac
Amadeusz Juskowiak <amadeusz@amanointeractive.com>
2016-12-03 16:24:10 +0100

    document evaluator

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -4,6 +4,7 @@
 use super::*;
 use polynomial::{Polynomial, PolynomialError};
 use std::collections::BTreeMap;
+use std::fmt;
 
 /// Pointer to function processing Polynomial arguments.
 ///
@@ -27,6 +28,13 @@ pub type FunctionHandle = Box<Fn(Vec<Polynomial>) -> Result<Polynomial, Evaluati
 pub struct Function {
   arity: usize,
   handle: FunctionHandle
+}
+
+/// Debug formatter for Function
+impl fmt::Debug for Function {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "Function(arity: {}, handle: ?)", self.arity)
+  }
 }
 
 impl Function {
@@ -88,6 +96,12 @@ impl Function {
 /// let parsed = parser.process(tokenizer.process("2+2")).unwrap();
 /// assert_eq!(evaluator.process(parsed), Ok(Polynomial::constant(4.0)));
 /// ```
+///
+/// # Extending
+///
+/// One can directly register functions or constants with the evaluator,
+/// or embed the evalutor in another TokensReducer which will add these
+/// handlers by default.
 pub struct Evaluator {
   functions: BTreeMap<String, Function>,
   constants: BTreeMap<String, Polynomial>
@@ -122,6 +136,109 @@ impl Default for Evaluator {
 /// with these arguments. Result of such evaluation is put back
 /// on the stack. In the end last value on the stack is returned
 /// as the result of the evaluation.
+///
+/// # Errors
+///
+/// A PolynomialError is returned when underlying function handler returns
+/// an error (it may happen as a result of converting non constant to float,
+/// division by zero of polynomials or division of polynomials with wrong
+/// degree).
+///
+/// A MultipleExpressions error is returned when there are multiple tokens
+/// left on the stack. It is causes by providing to many arguments to a
+/// function or giving too many expressions.
+///
+/// ```
+/// # use xxcalc::tokenizer::Tokenizer;
+/// # use xxcalc::parser::{Parser, Operator, OperatorAssociativity};
+/// # use xxcalc::evaluator::{Evaluator, Function};
+/// # use xxcalc::polynomial::Polynomial;
+/// # use xxcalc::{StringProcessor, TokensProcessor, TokensReducer, EvaluationError};
+/// let mut tokenizer = Tokenizer::default();
+/// let mut parser = Parser::default();
+/// let mut evaluator = Evaluator::default();
+///
+/// {
+///   let parsed = parser.process(tokenizer.process("2, 2")).unwrap();
+///   assert_eq!(evaluator.process(parsed).unwrap_err(), EvaluationError::MultipleExpressions);
+/// }
+///
+/// evaluator.register_function("foo", Function::new(1, Box::new(|args| {
+///   Ok(Polynomial::constant(42.0))
+/// })));
+///
+/// {
+///   let parsed = parser.process(tokenizer.process("foo(2, 2)")).unwrap();
+///   assert_eq!(evaluator.process(parsed).unwrap_err(), EvaluationError::MultipleExpressions);
+/// }
+/// ```
+///
+/// An ArgumentMissing error is returned when number of tokens on a stack
+/// is less than required arity of given functions. The error contains
+/// required arity and position of error.
+///
+/// ```
+/// # use xxcalc::tokenizer::Tokenizer;
+/// # use xxcalc::parser::{Parser, Operator, OperatorAssociativity};
+/// # use xxcalc::evaluator::{Evaluator, Function};
+/// # use xxcalc::polynomial::Polynomial;
+/// # use xxcalc::{StringProcessor, TokensProcessor, TokensReducer, EvaluationError};
+/// let mut tokenizer = Tokenizer::default();
+/// let mut parser = Parser::default();
+/// let mut evaluator = Evaluator::default();
+///
+/// parser.register_operator('+', Operator::new(1, OperatorAssociativity::Left));
+/// evaluator.register_function("+", Function::new(2, Box::new(|args| {
+///   // not a production code, just a sample
+///   Ok(args[0].clone() + args[1].clone())
+/// })));
+///
+/// {
+///   let parsed = parser.process(tokenizer.process("2+2+")).unwrap();
+///   assert_eq!(evaluator.process(parsed).unwrap_err(), EvaluationError::ArgumentMissing(String::from("+"), 2, 3));
+/// }
+///
+/// evaluator.register_function("foo", Function::new(1, Box::new(|args| {
+///   Ok(Polynomial::constant(42.0))
+/// })));
+///
+/// {
+///   let parsed = parser.process(tokenizer.process("foo()")).unwrap();
+///   assert_eq!(evaluator.process(parsed).unwrap_err(), EvaluationError::ArgumentMissing(String::from("foo"), 1, 0));
+/// }
+/// ```
+///
+/// An UnknownSymbol error is returned when an operator or identifier token is
+/// encountered with a name of unregistered function or constant. Each operator,
+/// function and constant need to be registered before it can be evaluated.
+///
+/// ```
+/// # use xxcalc::tokenizer::Tokenizer;
+/// # use xxcalc::parser::{Parser, Operator, OperatorAssociativity};
+/// # use xxcalc::evaluator::{Evaluator, Function};
+/// # use xxcalc::polynomial::Polynomial;
+/// # use xxcalc::{StringProcessor, TokensProcessor, TokensReducer, EvaluationError};
+/// let mut tokenizer = Tokenizer::default();
+/// let mut parser = Parser::default();
+/// let mut evaluator = Evaluator::default();
+///
+/// parser.register_operator('+', Operator::new(1, OperatorAssociativity::Left));
+///
+/// {
+///   let parsed = parser.process(tokenizer.process("2+2")).unwrap();
+///   assert_eq!(evaluator.process(parsed).unwrap_err(), EvaluationError::UnknownSymbol(String::from("+"), 1));
+/// }
+///
+/// {
+///   let parsed = parser.process(tokenizer.process("foo(1)")).unwrap();
+///   assert_eq!(evaluator.process(parsed).unwrap_err(), EvaluationError::UnknownSymbol(String::from("foo"), 0));
+/// }
+///
+/// {
+///   let parsed = parser.process(tokenizer.process("pi")).unwrap();
+///   assert_eq!(evaluator.process(parsed).unwrap_err(), EvaluationError::UnknownSymbol(String::from("pi"), 0));
+/// }
+/// ```
 impl TokensReducer for Evaluator {
   fn process(&self, tokens: &Tokens) -> Result<Polynomial, EvaluationError> {
     let mut stack: Vec<Polynomial> = Vec::with_capacity(10);
@@ -165,6 +282,132 @@ impl Evaluator {
     }
   }
 
+  /// Registers a function with its name.
+  ///
+  /// Each function (or an operator) must have a registered function
+  /// handle which takes arguments (or operands) and evaluate them
+  /// into a single value Polynomial.
+  //
+  /// If a function with the same name has been registered, previously
+  /// registered function is returned, however the name of the function
+  /// cannot collide with an already registered constant.
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// # use xxcalc::tokenizer::Tokenizer;
+  /// # use xxcalc::parser::{Parser, Operator, OperatorAssociativity};
+  /// # use xxcalc::evaluator::{Evaluator, Function};
+  /// # use xxcalc::polynomial::Polynomial;
+  /// # use xxcalc::{StringProcessor, TokensProcessor, TokensReducer, EvaluationError};
+  /// let mut tokenizer = Tokenizer::default();
+  /// let mut parser = Parser::default();
+  /// let mut evaluator = Evaluator::default();
+  ///
+  /// parser.register_operator('+', Operator::new(1, OperatorAssociativity::Left));
+  ///
+  /// {
+  ///   let parsed = parser.process(tokenizer.process("2+2")).unwrap();
+  ///   assert_eq!(evaluator.process(parsed), Err(EvaluationError::UnknownSymbol(String::from("+"), 1)));
+  /// }
+  ///
+  /// evaluator.register_function("+", Function::new(2, Box::new(|args| {
+  ///   // not a production code, just a sample
+  ///   Ok(args[0].clone() + args[1].clone())
+  /// })));
+  ///
+  /// {
+  ///   let parsed = parser.process(tokenizer.process("2+2")).unwrap();
+  ///   assert_eq!(evaluator.process(parsed), Ok(Polynomial::constant(4.0)));
+  /// }
+  /// ```
+  ///
+  /// # Errors
+  ///
+  /// A ConflictingName error is returned when name of the function collides
+  /// with previously registered constant.
+  ///
+  /// ```
+  /// # use xxcalc::evaluator::{Evaluator, Function};
+  /// # use xxcalc::polynomial::Polynomial;
+  /// # use xxcalc::{StringProcessor, TokensProcessor, TokensReducer, EvaluationError};
+  /// let mut evaluator = Evaluator::default();
+  ///
+  /// evaluator.register_constant("foo", Polynomial::constant(42.0));
+  /// let result = evaluator.register_function("foo", Function::new(1, Box::new(|args| {
+  ///   Ok(args[0].clone())
+  /// })));
+  ///
+  /// assert_eq!(result.unwrap_err(), EvaluationError::ConflictingName(String::from("foo")));
+  /// ```
+  pub fn register_function(&mut self, name: &str, function: Function) -> Result<Option<Function>, EvaluationError> {
+    if let Some(_) = self.constants.get(name) {
+      return Err(EvaluationError::ConflictingName(name.to_string()));
+    }
+
+    Ok(self.functions.insert(name.to_string(), function))
+  }
+
+  /// Registers a Polynomial constant with its name.
+  ///
+  /// An identifier with given name is replaced with the constant value
+  /// when it is encountered during evaluation process.
+  //
+  /// If a constant with the same name has been registered, previously
+  /// registered constant is returned, however the name of the constant
+  /// cannot collide with an already registered function.
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// # use xxcalc::tokenizer::Tokenizer;
+  /// # use xxcalc::parser::{Parser, Operator, OperatorAssociativity};
+  /// # use xxcalc::evaluator::{Evaluator, Function};
+  /// # use xxcalc::polynomial::Polynomial;
+  /// # use xxcalc::{StringProcessor, TokensProcessor, TokensReducer, EvaluationError};
+  /// let mut tokenizer = Tokenizer::default();
+  /// let mut parser = Parser::default();
+  /// let mut evaluator = Evaluator::default();
+  ///
+  /// {
+  ///   let parsed = parser.process(tokenizer.process("foo")).unwrap();
+  ///   assert_eq!(evaluator.process(parsed), Err(EvaluationError::UnknownSymbol(String::from("foo"), 0)));
+  /// }
+  ///
+  /// evaluator.register_constant("foo", Polynomial::constant(42.0));
+  ///
+  /// {
+  ///   let parsed = parser.process(tokenizer.process("foo")).unwrap();
+  ///   assert_eq!(evaluator.process(parsed), Ok(Polynomial::constant(42.0)));
+  /// }
+  /// ```
+  ///
+  /// # Errors
+  ///
+  /// A ConflictingName error is returned when name of the constant collides
+  /// with previously registered function.
+  ///
+  /// ```
+  /// # use xxcalc::evaluator::{Evaluator, Function};
+  /// # use xxcalc::polynomial::Polynomial;
+  /// # use xxcalc::{StringProcessor, TokensProcessor, TokensReducer, EvaluationError};
+  /// let mut evaluator = Evaluator::default();
+  ///
+  /// evaluator.register_function("foo", Function::new(1, Box::new(|args| {
+  ///   Ok(args[0].clone())
+  /// })));
+  /// let result = evaluator.register_constant("foo", Polynomial::constant(42.0));
+  ///
+  /// assert_eq!(result.unwrap_err(), EvaluationError::ConflictingName(String::from("foo")));
+  /// ```
+  pub fn register_constant(&mut self, name: &str, constant: Polynomial) -> Result<Option<Polynomial>, EvaluationError> {
+    if let Some(_) = self.functions.get(name) {
+      return Err(EvaluationError::ConflictingName(name.to_string()));
+    }
+
+    Ok(self.constants.insert(name.to_string(), constant))
+  }
+
   #[inline(always)]
   fn call_function(&self, name: &String, position: usize, stack: &mut Vec<Polynomial>) -> Result<Polynomial, EvaluationError> {
     if let Some(function) = self.functions.get(name) {
@@ -179,22 +422,6 @@ impl Evaluator {
     } else {
       Err(EvaluationError::UnknownSymbol(name.clone(), position))
     }
-  }
-
-  pub fn register_function(&mut self, name: &str, function: Function) -> Result<Option<Function>, EvaluationError> {
-    if let Some(_) = self.constants.get(name) {
-      return Err(EvaluationError::ConflictingName(name.to_string()));
-    }
-
-    Ok(self.functions.insert(name.to_string(), function))
-  }
-
-  pub fn register_constant(&mut self, name: &str, constant: Polynomial) -> Result<Option<Polynomial>, EvaluationError> {
-    if let Some(_) = self.functions.get(name) {
-      return Err(EvaluationError::ConflictingName(name.to_string()));
-    }
-
-    Ok(self.constants.insert(name.to_string(), constant))
   }
 }
 
